506be36bef854a0351c6907ace9a64a5415ee648
Ivan Arellano <ivares.misaden@hotmail.com>
2022-10-29 18:45:11 -0500

    Add methods example

--- minimal/WebApplication1/Program.cs
+++ minimal/WebApplication1/Program.cs
@@ -2,33 +2,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("get-example", () => "Hello get" );
+app.MapPost("post-example", () => "Hello post");
+app.MapGet("ok-object", () => Results.Ok(new
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    Message = "Hello ok object"
+}));
+app.MapGet("asyn-response", async () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    await Task.Delay(1000);
+    return Results.Ok(new
+    {
+        Message = "Hello ok async"
+    });
 });
+
+app.MapGet("get-params/{age:int}", (int age) => $"Age provided was {age}");
+app.MapGet("cars/{carId:regex(^[a-z0-9]+$)}", (string carId) => $"Card id provided was: {carId}");
+app.MapGet("books/{isbn:length(13)}", (string isbn) => $"ISBN was: {isbn}");
+
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
